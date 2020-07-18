8e81c4a4a95ed95f46116b6ccdeace48143987aa
Natasha Gallagher <nbhat1@gmail.com>
2020-07-18 19:00:01 +0000

    Added Addition

--- calculator.py
+++ calculator.py
@@ -8,8 +8,12 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 while True:
     input_string = input("Enter your question: ")
     tokens = input_string.split(' ')
+    num1 = float(tokens[1])
+    num2 = float (tokens[2])
+
     if tokens[0] == 'q':
         print("You will exit")
         break
     else:
-        print ("x") 
+        if(tokens[0] == "+"):
+            print(add(num1, num2))
